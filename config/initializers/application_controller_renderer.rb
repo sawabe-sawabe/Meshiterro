4b62af7a0b0976e52b26d0a3cfbf433c02e57fb0
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-05-07 14:28:09 +0000

    [start]5-5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
+Refile.secret_key = '5bca0b24b65611aeb821185898bbc691f454e1908e583c55ee82de143a8e673620edaee96e1dd4a6da675179b231f6c8da7438b55afa937b94c145c3b2ce0b80'
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
